d034781a3bf3b146bbcb808dabcff6c0beff788c
AdamBalski <adambalski1@gmail.com>
2023-06-21 17:34:41 +0200

    INITIAL: FIRST COMMIT

---
+++ src/main/java/com/finman/finmanbackend/FinmanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finman.finmanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinmanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinmanBackendApplication.class, args);
+    }
+
+}
