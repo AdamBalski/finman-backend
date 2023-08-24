377ef54de75fb4aee69498aa0367d4ae1081ad75
AdamBalski <adambalski1@gmail.com>
2023-08-24 16:21:15 +0200

    UPDATE: setup.sql

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -5,4 +5,16 @@ create table public.app_user (
     password varchar(255) not null,
     role varchar(255) not null check (role in ('STANDARD','ADMIN')),
     primary key (id)
-)
+);
+
+-- refresh_token - RefreshToken.class
+CREATE TABLE refresh_token
+(
+    id      UUID                        NOT NULL,
+    user_id UUID                        NOT NULL,
+    exp     TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    CONSTRAINT pk_refresh_token PRIMARY KEY (id)
+);
+
+ALTER TABLE refresh_token
+    ADD CONSTRAINT FK_REFRESH_TOKEN_ON_USER FOREIGN KEY (user_id) REFERENCES public.app_user (id) ON DELETE CASCADE;
